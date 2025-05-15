ea72780aa39d0806129d676dfaa1d3d80b576760
Kazioslaw <Kamil_d13@wp.pl>
2025-05-15 21:58:58 +0200

    Created category controller. Configured XML documentation support in Swagger.

---
+++ ComputerPartsShop/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using ComputerPartsShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerPartsShop.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CategoryController : ControllerBase
+	{
+		static List<Category> Categories = new()
+		{
+			new Category { ID = 1, Name = "CPU/Processor" },
+			new Category { ID = 2, Name = "RAM Memory" },
+			new Category { ID = 3, Name = "Motherboard"},
+			new Category { ID = 4, Name = "GPU/Graphics Card"},
+			new Category { ID = 5, Name = "Computer Case"},
+			new Category { ID = 6, Name = "Power Supply"},
+			new Category { ID = 7, Name = "Hard Drive"},
+			new Category { ID = 8, Name = "Solid State Drive"}
+		};
+
+		/// <summary> 
+		/// Get method to get list of categories
+		/// </summary>
+		/// <returns>
+		/// List of categories.
+		/// </returns>
+		[HttpGet]
+		public ActionResult<List<Category>> GetCategories()
+		{
+			return Ok(Categories);
+		}
+
+		/// <summary>
+		/// Get method to get category by its id
+		/// </summary>
+		/// <param name="id">Category id</param>
+		/// <returns>Category by its id</returns>
+
+		[HttpGet("{id}")]
+		public ActionResult<Category> GetCategory(int id)
+		{
+			var category = Categories.FirstOrDefault(c => c.ID == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return Ok(category);
+		}
+
+		/// <summary>
+		/// Post method to create new Category
+		/// </summary>
+		/// <param name="category"></param>
+		/// <returns>Newly created category with id.</returns>
+
+		[HttpPost]
+		public ActionResult CreateCategory(Category category)
+		{
+			category.ID = Categories.Count + 1;
+			Categories.Add(category);
+			return CreatedAtAction(nameof(CreateCategory), category);
+		}
+
+		/// <summary>
+		/// Put method to update category by its id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="updatedCategory"></param>
+		/// <returns>Properly updated category</returns>
+
+		[HttpPut("{id}")]
+		public ActionResult<Category> UpdateCategory(int id, Category updatedCategory)
+		{
+			var category = Categories.FirstOrDefault(c => c.ID == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			category.Name = updatedCategory.Name;
+
+			return Ok(category);
+		}
+
+		/// <summary>
+		/// Delete method to delete category by its id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>
+		/// Information about successful deletion
+		/// </returns>
+
+		[HttpDelete("{id}")]
+		public ActionResult DeleteCategory(int id)
+		{
+			var category = Categories.FirstOrDefault(c => c.ID == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			Categories.Remove(category);
+
+			return Ok();
+		}
+	}
+}
--- ComputerPartsShop/Program.cs
+++ ComputerPartsShop/Program.cs
@@ -12,7 +12,12 @@ namespace ComputerPartsShop
 			builder.Services.AddControllers();
 			// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+			builder.Services.AddSwaggerGen(c =>
+			{
+				var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+				c.IncludeXmlComments(xmlPath);
+			});
 
 			var app = builder.Build();
 
